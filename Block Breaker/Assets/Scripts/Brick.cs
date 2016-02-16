8fdd34be9cbd0df4db584e3d40b4fa868537024f
vygis83 <vygis83@gmail.com>
2016-02-16 14:35:32 +0000

    Complete Game

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -6,6 +6,7 @@ public class Brick : MonoBehaviour {
 	public AudioClip crack;
 	public Sprite[] hitSprites;
 	public static int breakableCount = 0;
+	public GameObject smoke;
 
 	private int timesHit;
 	private LevelManager levelManager; 
@@ -39,6 +40,8 @@ public class Brick : MonoBehaviour {
 		if (timesHit >= maxHits) {
 			breakableCount--;
 			levelManager.BrickDestroyed();
+			GameObject puff = Instantiate(smoke, transform.position, Quaternion.identity) as GameObject;
+			puff.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
 			Destroy (gameObject);
 		} else {
 			LoadSprites();
@@ -49,6 +52,8 @@ public class Brick : MonoBehaviour {
 		int spriteIndex = timesHit - 1;
 		if (hitSprites [spriteIndex]) {
 			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		} else {
+			Debug.LogError ("Brick sprite missing");
 		}
 	}
 
